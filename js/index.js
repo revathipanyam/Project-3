268d522206185830b59add963d12499b162474b1
revathipanyam <112520998+revathipanyam@users.noreply.github.com>
2022-11-22 17:05:33 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: { lat: 14.675796515578558, lng: 77.59489702704062},
+    gestureHandling: "none",
+    zoomControl: true,
+    scaleControl: true,
   });
   
   var marker = new google.maps.Marker({
