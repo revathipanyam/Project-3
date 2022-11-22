14f8d1bf1c336414e8292a0940b964cb59442aa3
revathipanyam <112520998+revathipanyam@users.noreply.github.com>
2022-11-22 16:51:59 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: { lat: 14.675796515578558, lng: 77.59489702704062},
+  });
+  
+  var marker = new google.maps.Marker({
+    position: { lat: 14.675796515578558, lng: 77.59489702704062 },
+    map: map,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content:"My Hometown"
+  });
+  infowindow.open(map, marker);
+}
+window.initMap = initMap;
