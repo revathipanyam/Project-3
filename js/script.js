51ed0f1f6fa1f50c7d057c0e407beea2ed99b1ee
revathipanyam <112520998+revathipanyam@users.noreply.github.com>
2022-11-22 13:59:55 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 14.727355574823836, lng: 77.59688877403104},
+    zoom: 15,
+	zoomControl: false,
+    scaleControl: true,
+	mapTypeId: 'hybrid'
+  });
+  
+  var marker = new google.maps.Marker({
+    position: { lat: 14.727355574823836, lng: 77.59688877403104},
+    map: map,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content:"My Hometown"
+  });
+  infowindow.open(map, marker);
+}
+window.initMap = initMap;
